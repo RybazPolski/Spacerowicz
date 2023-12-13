f5f4ad06c73d7b148a9a183668973059da5cf655
RybazPolski <julorybarczyk@gmail.com>
2023-12-13 18:28:06 +0100

    init for branch and a few files

---
+++ app/src/main/java/com/rybarczykzsl/spacerowicz/IWillDoWhatIMustActivity.java
@@ -0,0 +1,14 @@
+package com.rybarczykzsl.spacerowicz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IWillDoWhatIMustActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_will_do_what_i_must);
+    }
+}
